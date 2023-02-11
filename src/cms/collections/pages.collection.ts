a08e87d30dcda16ab7b7fe2d393efc4cc205f7de
Kristo Lippur <kristo@elnino.tech>
2023-02-11 21:50:20 +0100

    Add validation for slug uniqueness

--- src/cms/collections/pages.collection.ts
+++ src/cms/collections/pages.collection.ts
@@ -18,6 +18,20 @@ export const Pages: CollectionConfig = {
       type: 'text',
       unique: true,
       required: true,
+      validate: async (value: string, { payload, id }) => {
+        if (!payload) return true
+
+        const conflicts = await payload.find<'pages'>({
+          collection: 'pages',
+          where: { slug: { equals: value }, and: [{ id: { not_equals: id } }] },
+        })
+
+        if (conflicts.docs.length > 0) {
+          return `This slug is already in use by page "${conflicts.docs[0].title}"`
+        }
+
+        return true
+      },
       admin: {
         components: {
           Field: SlugField,
